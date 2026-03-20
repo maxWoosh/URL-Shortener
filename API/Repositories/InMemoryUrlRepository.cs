c9d35eb9a1f0e611fec1aa91c0c0071c8363ca41
maxWoosh <maksimgg4@gmail.com>
2026-03-20 14:33:58 +0300

    Добавлен слой репозиториев (In-memory реализация)

---
+++ API/Repositories/InMemoryUrlRepository.cs
@@ -0,0 +1,55 @@
+﻿using API.Models.Entities;
+using API.Repositories.Interfaces;
+using System.Collections.Concurrent;
+
+namespace API.Repositories
+{
+    public class InMemoryUrlRepository : IUrlRepository
+    {
+        // Хранилище данных
+        private readonly ConcurrentDictionary<string, ShortenedUrl> _urls = new();
+
+        // Защита от одновременных запросов, однако реализовал через ConcurrentDictionary
+        private readonly object _locker = new();
+
+        public Task<ShortenedUrl?> GetByCodeAsync(string shortCode)
+        {
+            _urls.TryGetValue(shortCode, out var url);
+            return Task.FromResult(url);
+
+        }
+
+        public Task<ShortenedUrl?> GetByLongUrlAsync(string longUrl)
+        {
+            var url = _urls.Values.FirstOrDefault(u => u.OriginalUrl == longUrl);
+            return Task.FromResult(url);
+        }
+
+        public Task<ShortenedUrl> CreateAsync(ShortenedUrl shortenedUrl)
+        {
+            if (_urls.TryAdd(shortenedUrl.ShortCode, shortenedUrl))
+                throw new InvalidOperationException($"Код {shortenedUrl.ShortCode} уже существует.");
+            return Task.FromResult(shortenedUrl);
+        }
+
+        public Task UpdateAsync(ShortenedUrl shortenedUrl)
+        {
+            if (!_urls.ContainsKey(shortenedUrl.ShortCode))
+                throw new InvalidOperationException($"Код {shortenedUrl.ShortCode} не найден.");
+            _urls[shortenedUrl.ShortCode] = shortenedUrl;
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> CodeExistsAsync(string shortCode)
+        {
+            return Task.FromResult(_urls.ContainsKey(shortCode));
+        }
+
+        public Task<IEnumerable<ShortenedUrl>> GetAllAsync()
+        {
+            // Values возвращает snapshot на момент вызова
+            return Task.FromResult(_urls.Values.AsEnumerable());
+        }
+
+    }
+}
---
+++ API/Repositories/Interfaces/IUrlRepository.cs
@@ -0,0 +1,25 @@
+﻿using API.Models.Entities;
+
+namespace API.Repositories.Interfaces
+{
+    public interface IUrlRepository
+    {
+        // Получить ссылку по короткому коду
+        Task<ShortenedUrl?> GetByCodeAsync(string shortCode);
+
+        // Получить ссылку по оригинальному Url
+        Task<ShortenedUrl?> GetByLongUrlAsync(string longUrl);
+
+        // Создать новую ссылку
+        Task<ShortenedUrl> CreateAsync(ShortenedUrl shortenedUrl);
+
+        // Обновить существующую ссылку (для счетчика)
+        Task UpdateAsync(ShortenedUrl shortenedUrl);
+
+        // Проверить, существует ли код
+        Task<bool> CodeExistsAsync(string shortCode);
+
+        // Получить все сущности
+        Task<IEnumerable<ShortenedUrl>> GetAllAsync();
+    }
+}
