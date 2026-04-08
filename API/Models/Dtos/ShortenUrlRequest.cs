570a2108837fde888d56aa1132554ac1dcfbdef5
maxWoosh <maksimgg4@gmail.com>
2026-04-08 15:37:13 +0300

    Добавлен  слой DTO

---
+++ API/Models/Dtos/ShortenUrlRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Dtos
+{
+    public class ShortenUrlRequest
+    {
+        [Required(ErrorMessage = "Необходима URL ссылка")]
+        [Url(ErrorMessage = "Некорректный формат ссылки")]
+        public string Url { get; set; } = string.Empty;
+    }
+}
---
+++ API/Models/Dtos/ShortenUrlResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models.Dtos
+{
+    public class ShortenUrlResponse
+    {
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortCode { get; set; } = string.Empty;
+        public string ShortUrl { get; set; } = string.Empty;
+    }
+}
