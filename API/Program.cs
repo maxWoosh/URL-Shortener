0f486e9406e5e307e593199583200b902ca9a550
maxWoosh <maksimgg4@gmail.com>
2026-04-15 14:30:40 +0300

    Настройка Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,41 @@
+using API.Repositories;
+using API.Repositories.Interfaces;
+using API.Services;
+using API.Services.Interfaces;
+using API.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Build();
+// Добавляем контроллер
+builder.Services.AddControllers();
+
+// Настраиваем Swagger/OpenAPI (для тестирования)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+# region Регистрация зависимостей
+// Репозиторий (in-memory)
+builder.Services.AddScoped<IUrlRepository, InMemoryUrlRepository>();
+
+// Генератор кодов
+builder.Services.AddScoped<CodeGenerator>();
+
+// Сервис
+builder.Services.AddScoped<IUrlService, UrlService>();
+# endregion
+
+var app = builder.Build();
+
+// Конвейер обработки запросов
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
+
 
